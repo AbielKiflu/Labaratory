4d7992df1968ee4e246dd1106ac03479e8ef7102
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-06-29 22:47:08 +0200

    Project init

---
+++ EntityTest/Practice/Program.cs
@@ -0,0 +1,7 @@
+﻿using Practice;
+
+Tasking tasking = new Tasking(new List<string> { "Abiel", "Aman", "Stiff", "Haben" });
+
+tasking.ListPeople();
+
+Console.ReadKey();
---
+++ EntityTest/Practice/Tasking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Tasking
+    {
+
+        public List<string> People { get; set; }
+
+
+        public Tasking(List<string> people) { 
+            this.People = people;
+        }
+
+
+        public void ListPeople()
+        {
+            foreach (var person in this.People)
+            {
+                Console.WriteLine(person);
+            }
+        }
+
+
+    }
+}
